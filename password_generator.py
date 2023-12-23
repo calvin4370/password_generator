6c9a9628283676e874702cafd9ab47a353f8ec5b
calvin4370 <calvin.j.j.chan@gmail.com>
2023-12-23 17:56:09 +0800

    Changed Version code

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,6 @@
 import random
 
-""" Version 23/12/23 B
+""" Version 23/12/23 C
                          NOTES ON STRONG PASSWORDS
 ---------------------------------------------------------------------------
 - at least 12 characters but 14 or more is better
